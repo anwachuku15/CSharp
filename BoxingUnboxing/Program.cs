5f743f8d55034d74c11492434307c2bb1749603e
anwachuku15 <45902243+anwachuku15@users.noreply.github.com>
2019-05-06 15:24:47 -0400

    Add files via upload

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            List<object> obj = new List<object>();
+            obj.Add(7);
+            obj.Add(28);
+            obj.Add(-1);
+            obj.Add(true);
+            obj.Add("chair");
+
+            foreach(object i in obj){
+                Console.WriteLine(i);
+            }
+            int sum = 0;
+
+            foreach(object i in obj){
+                if(i is int){
+                    int iValue = (int)i;
+                    sum += iValue;
+                }
+            }
+            Console.WriteLine(sum);
+
+
+        }
+    }
+}
